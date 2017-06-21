d8c2a76c9816a4e3ba9d7bc9935638f5467612a7
PierreBougon <pierre.bougon@epitech.eu>
2017-06-21 02:36:17 +0200

    Main

---
+++ ai/src/main.cpp
@@ -0,0 +1,27 @@
+#include <exception>
+#include <tools/Logger.hpp>
+#include <network/Client.hpp>
+
+/*
+** main.c for ai in /home/pierre/rendu/TEK2/PSU_2016_zappy/ai
+**
+** Made by PierreBougon
+** Login   <pierre@epitech.net>
+**
+** Started on  Sat Jun 10 21:52:31 2017 PierreBougon
+** Last update Sat Jun 10 22:30:54 2017 PierreBougon
+*/
+
+int	main(int ac, char **av)
+{
+    try
+    {
+        zappy::network::Client::getInstance().Init(static_cast<uint16_t>(std::atoi(av[1])), av[2]);
+        zappy::network::Client::getInstance().connect();
+    }
+    catch (std::exception const &exception)
+    {
+        Logger::log(Logger::DEBUG, exception.what());
+    }
+    return (0);
+}
