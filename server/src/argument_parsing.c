06c6b33f7dd39c2f30074065484d8a45bb294790
marc brout <marc.brout@epitech.eu>
2017-06-23 17:07:22 +0200

    arg_parsing

--- server/src/argument_parsing.c
+++ server/src/argument_parsing.c
@@ -3,54 +3,51 @@
 #include "arguments/arguments.h"
 #include "arguments/config.h"
 
-static t_arg commands[7] =
-    {
-        { "-p", argument_port, false },
-        { "-x", argument_width, false },
-        { "-y", argument_height, false },
-        { "-n", argument_name, false },
-        { "-c", argument_nbclients, false },
-        { "-t", argument_time, false },
-        { NULL, NULL, false }
-    };
+static t_arg commands[7] = {{"-p", argument_port, false},
+                            {"-x", argument_width, false},
+                            {"-y", argument_height, false},
+                            {"-n", argument_name, false},
+                            {"-c", argument_nbclients, false},
+                            {"-t", argument_time, false},
+                            {NULL, NULL, false}};
 
-static int arguments_validation()
+static int arguments_validation(char **args)
 {
   t_arg *it;
 
   it = commands;
   while (it->exec != NULL)
-  {
-    if (!it->ok)
-      return (1);
-    ++it;
-  }
+    {
+      if (!it->ok)
+	return (print_help(args[0]));
+      ++it;
+    }
   return (0);
 }
 
 int process_command_line(t_config *config, int ac, char **args)
 {
-  int argument;
+  int    argument;
   t_arg *it;
 
   argument = 1;
   while (argument < ac)
-  {
-    it = commands;
-    while (it->exec != NULL)
     {
-      if (!strcmp(it->cmd, args[argument]))
-      {
-        if (it->exec(config, ac, args, &argument))
-          return (print_help(args[0]) || EXIT_FAILURE);
-        it->ok = true;
-        break;
-      }
-      ++it;
+      it = commands;
+      while (it->exec != NULL)
+	{
+	  if (!strcmp(it->cmd, args[argument]))
+	    {
+	      if (it->exec(config, ac, args, &argument))
+		return (print_help(args[0]) || EXIT_FAILURE);
+	      it->ok = true;
+	      break;
+	    }
+	  ++it;
+	}
+      if (it->exec == NULL)
+	return (print_help(args[0]) || EXIT_FAILURE);
+      ++argument;
     }
-    if (it->exec == NULL)
-      return (print_help(args[0]) || EXIT_FAILURE);
-    ++argument;
-  }
-  return (arguments_validation());
+  return (arguments_validation(args));
 }
