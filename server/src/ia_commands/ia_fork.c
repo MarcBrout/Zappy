d36806c7ff2822e3f9ce780e752641a7105ea3c7
marc brout <marc.brout@epitech.eu>
2017-06-25 01:58:32 +0200

    norme arguments

--- server/src/ia_commands/ia_fork.c
+++ server/src/ia_commands/ia_fork.c
@@ -13,6 +13,6 @@ int	ia_fork(t_server *server, ID id, char *cmd)
   if (add_egg(server, &server->game.clients[id].ia.pos,
 	      server->game.clients[id].ia.team))
     return (1);
-  strncircular(&server->game.clients[id].w, "ok\n", strlen("ok\n"));
+  send_to_ia(server, id, "ok\n");
   return (0);
 }
