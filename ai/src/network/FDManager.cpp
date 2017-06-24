5121f6f3dc81966b77ba28e55a2025b4d5402b9f
PierreBougon <pierre.bougon@epitech.eu>
2017-06-24 17:42:08 +0200

    FDManager : Create a FDManager to ba able to easily deal with set of file descriptor this encapsulate FD_* + select

---
+++ ai/src/network/FDManager.cpp
@@ -0,0 +1,56 @@
+//
+// Created by Pierre Bougon on 23/06/17.
+//
+
+#include "network/FDManager.hpp"
+
+zappy::FDManager::FDManager()
+{
+    reset_fds();
+}
+
+void zappy::FDManager::reset_fds()
+{
+    FD_ZERO(&_rfds);
+    FD_ZERO(&_wfds);
+}
+
+zappy::FDManager::~FDManager()
+{
+
+}
+
+void zappy::FDManager::manageFD(int fd)
+{
+    reset_fds();
+    if (fd > -1)
+    {
+        FD_SET(fd, &_wfds);
+        FD_SET(fd, &_rfds);
+    }
+}
+
+int zappy::FDManager::select(int fd)
+{
+    if (fd == -1)
+        return (0);
+    return (select(fd + 1, &_rfds, &_wfds, NULL, &_tv));
+}
+
+const timeval &zappy::FDManager::get_tv() const
+{
+    return _tv;
+}
+
+void zappy::FDManager::set_tv(unsigned int usec)
+{
+    FDManager::_tv.tv_usec = usec;
+}
+
+bool zappy::FDManager::isFdSet(int fd, zappy::FDManager::FDType fdType)
+{
+    if (fdType == READ)
+        return FD_ISSET(fd, &_rfds);
+    else
+        return FD_ISSET(fd, &_wfds);
+}
