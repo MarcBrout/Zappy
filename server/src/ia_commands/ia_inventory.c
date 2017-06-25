7ee578375d0a92f42b5c47389de2537d5fdfc993
Edouard <edouard.puillandre@epitech.eu>
2017-06-25 17:04:11 +0200

    add ia_inventory

---
+++ server/src/ia_commands/ia_inventory.c
@@ -0,0 +1,35 @@
+//
+// Created by puilla_e on 09/06/17.
+//
+
+#include "server/send.h"
+
+static const char *obj_tab[OBJ_COUNT] =
+ {
+  "food",
+  "linemate",
+  "deraumere",
+  "sibur",
+  "mendiane",
+  "phiras",
+  "thystame"
+ };
+
+int		ia_inventory(t_server *server, ID id, char *cmd)
+{
+  Object	obj;
+  t_client	*client;
+
+  (void) cmd;
+  obj = FOOD;
+  client = &server->game.clients[id];
+  send_to_ia(server, id, "[%s %d", obj_tab[obj], client->ia.inventory[obj]);
+  ++obj;
+  while (obj < THYSTAME)
+    {
+      send_to_ia(server, id, ",%s %d", obj_tab[obj], client->ia.inventory[obj]);
+      ++obj;
+    }
+  send_to_ia(server, id, ",%s %d]\n", obj_tab[obj], client->ia.inventory[obj]);
+  return (0);
+}
