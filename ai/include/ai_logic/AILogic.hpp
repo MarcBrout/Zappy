eb360a4e6e067a2fc6b84358a22d3e1831632903
marc brout <marc.brout@epitech.eu>
2017-06-26 13:52:40 +0200

    Creating client_ia_logic

---
+++ ai/include/ai_logic/AILogic.hpp
@@ -0,0 +1,13 @@
+//
+// Created by brout_m on 26/06/17.
+//
+
+#ifndef ZAPPY_AILOGIC_HPP
+#define ZAPPY_AILOGIC_HPP
+
+class AILogic
+{
+
+};
+
+#endif // ZAPPY_AILOGIC_HPP
---
+++ ai/src/ai_logic/AILogic.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brout_m on 26/06/17.
+//
+
+#include "ai_logic/AILogic.hpp"
